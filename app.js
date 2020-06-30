1f0280228d8cde327794ce444acc21b543fab4ec
Mateusz Skalniak <mskalniak@gmail.com>
2020-06-30 20:37:24 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Counter app');
+
+    const container = document.querySelector('.container');
+    container.style.borderTop = '1px solid blue';
+});
